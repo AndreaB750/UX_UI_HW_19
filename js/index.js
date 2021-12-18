aab69c35abcb4e466fb18a476f2cb5645c6e1914
Andrea Bouchan <66983091+AndreaB750@users.noreply.github.com>
2021-12-18 15:37:12 -0600

    interaction

---
+++ js/index.js
@@ -0,0 +1,23 @@
+
+console.log("your file is loading correctly");
+
+$('.navabout').hover(function(){
+    $(this).css("font-weight", "bold");
+},
+function(){
+    $(this).css("font-weight", "normal");
+});
+
+$('.navwork').hover(function(){
+    $(this).css("font-weight", "bold");
+},
+function(){
+    $(this).css("font-weight", "normal");
+});
+
+$('.navcontact').hover(function(){
+    $(this).css("font-weight", "bold");
+},
+function(){
+    $(this).css("font-weight", "normal");
+});
